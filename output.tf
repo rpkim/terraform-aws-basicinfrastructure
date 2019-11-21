3413bc18a44e80f5a966c9b3b0acbd8063ca4407
Jay Kim <rpkim.jay@gmail.com>
2019-11-21 14:22:17 +0900

    update description

--- output.tf
+++ output.tf
@@ -1,40 +1,49 @@
-# Outputs needed by other resources
 output "id" {
-  value = "${aws_vpc.vpc.id}"
+  value       = "${aws_vpc.vpc.id}"
+  description = "vpc id"
 }
 
 output "cidr_block" {
-  value = "${aws_vpc.vpc.cidr_block}"
+  value       = "${aws_vpc.vpc.cidr_block}"
+  description = "vpc cidr block"
 }
 
 output "db_route_tables" {
-  value = ["${aws_route_table.db.*.id}"]
+  value       = ["${aws_route_table.db.*.id}"]
+  description = "db route table ids"
 }
 
 output "db_subnets" {
-  value = ["${aws_subnet.db.*.id}"]
+  value       = ["${aws_subnet.db.*.id}"]
+  description = "db subnets id"
 }
 
 output "private_route_tables" {
-  value = ["${aws_route_table.private.*.id}"]
+  value       = ["${aws_route_table.private.*.id}"]
+  description = "private subnet route table ids"
 }
 
 output "private_subnets" {
-  value = ["${aws_subnet.private.*.id}"]
+  value       = ["${aws_subnet.private.*.id}"]
+  description = "private subnets id"
 }
 
 output "public_route_table" {
-  value = "${aws_route_table.public.id}"
+  value      = "${aws_route_table.public.id}"
+  descrtpion = "public route table id"
 }
 
 output "public_subnets" {
-  value = ["${aws_subnet.public.*.id}"]
+  value       = ["${aws_subnet.public.*.id}"]
+  description = "public subnets id"
 }
 
 output "nat_eips" {
-  value = ["${aws_eip.nat_eips.*.public_ip}"]
+  value       = ["${aws_eip.nat_eips.*.public_ip}"]
+  description = "nat eips"
 }
 
 output "igw_id" {
-  value = "${aws_internet_gateway.igw.id}"
+  value       = "${aws_internet_gateway.igw.id}"
+  description = "internet gateway id"
 }
--- variables.tf
+++ variables.tf
@@ -1,27 +1,34 @@
 variable name {
-  type = "string"
+  type        = "string"
+  description = "service name"
 }
 
 variable cidr_block {
-  type = "string"
+  type        = "string"
+  description = "vpc cidr block"
 }
 
 variable zones {
-  type = "list"
+  type        = "list"
+  description = "zones"
 }
 
 variable public_subnets {
-  type = "list"
+  type        = "list"
+  description = "public subnets"
 }
 
 variable private_subnets {
-  type = "list"
+  type        = "list"
+  description = "private subnets"
 }
 
 variable db_subnets {
-  type = "list"
+  type        = "list"
+  description = "db subnets"
 }
 
 variable region {
-  type = "string"
+  type        = "string"
+  description = "region"
 }
